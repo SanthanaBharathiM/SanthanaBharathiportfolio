9bddb71e7ed8751febedc9700cf7a13cc40b3d35
Santhana Bharathi M <114559700+SanthanaBharathiM@users.noreply.github.com>
2025-05-25 14:57:21 +0530

    Update scriptpro.js

--- scriptpro.js
+++ scriptpro.js
@@ -47,23 +47,23 @@ window.onscroll = () => {
     
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement. scrollHeight);
 }
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyTklRtm6l6cwL-8nvBx-ZSVuxFRSoEs1c6A3unJuw7x9jl3-hXTnxny5Xlxz3X51DQLA/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyTklRtm6l6cwL-8nvBx-ZSVuxFRSoEs1c6A3unJuw7x9jl3-hXTnxny5Xlxz3X51DQLA/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById("msg")
 
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML = "Message sent sucessfully &#x2713"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-});
+//   form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//       .then(response => {
+//         msg.innerHTML = "Message sent sucessfully &#x2713"
+//         setTimeout(function(){
+//             msg.innerHTML = ""
+//         },5000)
+//         form.reset()
+//       })
+//       .catch(error => console.error('Error!', error.message))
+// });
 
 function myFunction() {
     var dots = document.getElementById("dots");
